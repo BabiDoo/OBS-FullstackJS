d8d7280b3da0d1b244a477334bb4ec366cdbf8fe
Barbara <barbara.laisdorneles@gmail.com>
2025-07-02 17:54:41 -0300

    Update index.js
    fix keyboard validations andf comments

--- WEB_BASIC/4-JAVASCRIPT/Javascript-II/ProjetoPratico-Calc-JS/index.js
+++ WEB_BASIC/4-JAVASCRIPT/Javascript-II/ProjetoPratico-Calc-JS/index.js
@@ -24,40 +24,32 @@ const allowedKeys = [
   " ",
 ];
 
-
-
-
 /////////////////////////PARA CLICK
 //seleciona os botoes
-document.querySelectorAll(".charKey").forEach(charkeyOn); //seleciona elementos da classe charkey e para cada um ele invoca a funcao charkeyOn
-
+document.querySelectorAll(".charKey").forEach(charkeyOn); //seleciona elementos da classe charkey e para cada um ele invoca a funcao charkeyOn()
 
 //Limpa o input
-document.getElementById("clear").addEventListener("click", clearInput); //seleciona o elemento com id clear e adiciona o ev listener de click para invocar a função clearInput)
+document.getElementById("clear").addEventListener("click", clearInput); //seleciona o elemento com id clear e adiciona o ev listener de click para invocar a função clearInput())
 
 //Calcula o resultado
-document.getElementById("equal").addEventListener("click", calculate);
-
+document.getElementById("equal").addEventListener("click", calculate); //seleciona o elemento com id equal e add o ev listener de click para invocar a funcao calculate()
 
 ////////////////////////////PARA TECLADO
-input.addEventListener("keydown", ev => {
-    ev.preventDefault();
-        if(allowedKeys.includes(ev.key)) {
-            input.value += ev.key;
-            return;
-        }
-        if (ev.key === "Backspace") {
-            input.value = input.value.slice(0, -1);
-            return;
-        }
-        if(ev.key === "Enter") {
-            calculate();
-            return;
-        }
-
-})
-    
-
+input.addEventListener("keydown", (ev) => { //add um ev listener de keydown no input que faz um procedimento de acordo com a key
+  ev.preventDefault();
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key;
+    return;
+  }
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1);
+    return;
+  }
+  if (ev.key === "Enter") {
+    calculate();
+    return;
+  }
+});
 
 /////////////////////FUNÇÕES
 function clearInput() {
@@ -75,7 +67,7 @@ function charkeyOn(btn) {
   });
 }
 
-function calculate () {
-    const result = eval(input.value);
-    resultInput.value = result;
+function calculate() {
+  const result = eval(input.value); // guarda o valor do input passado por eval para a const resultado
+  resultInput.value = result; //seta o valor de resultInput com o valor que foi avaliado no eval()
 }
